bf97e1e2a53f9eb30091361d6c7f68e0a4dfadb1
chamocell <venemexcoin@gmail.com>
2020-03-14 21:53:49 -0600

    inicio de clon

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app1')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+        <h2>Title</h2>
+        <p>Posted 5 hours ago.</p>
+    </div>
+</div>
+<hr>
+
+<div class="row">
+    <div class="col-md-12">
+        <h2>Title</h2>
+        <p>Posted 5 hours ago.</p>
+    </div>
+</div>
+<hr>
+
+<div class="row">
+    <div class="col-md-12">
+        <h2>Title</h2>
+        <p>Posted 5 hours ago.</p>
+    </div>
+</div>
+<hr>
+
+@endsection
+
+
